7c91ee3c82bb50912c3d383bd9491d605e24f9a1
Lauren Tanea <laurentanea@Lauren-Taneas-MacBook-Air.local>
2022-11-18 12:22:29 +0530

    Removed prepopulated value from the iamge url input

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -11,8 +11,6 @@ const movieList = document.getElementById('movie-list');
 const ratingInput = document.getElementById('rating');
 const titleInput = document.getElementById('title');
 const imageUrl = document.getElementById('image-url');
-imageUrl.value = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/512px-Unofficial_JavaScript_logo_2.svg.png?20141107110902';
-
 
 // Initially the movies array will be empty
 const movies =  [];
